16dff06e264eceaa3dd254b123cc3eab51e87304
sulowskikarol <karolsu128@gmail.com>
2024-06-04 14:12:17 +0200

    Fixed deleting observation

--- src/main/java/com/partypilot/api/repository/ObservationRepository.java
+++ src/main/java/com/partypilot/api/repository/ObservationRepository.java
@@ -2,6 +2,8 @@ package com.partypilot.api.repository;
 
 import com.partypilot.api.model.Observation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -9,6 +11,8 @@ public interface ObservationRepository extends JpaRepository<Observation, Long> 
 
     List<Observation> findObservationsByUserId(Long userId);
 
+    @Modifying
+    @Transactional
     void deleteByEventId(Long eventId);
 
     boolean existsByEventIdAndUserId(Long eventId, Long userId);
